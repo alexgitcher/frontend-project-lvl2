1abb7f88969fa3fbd5fff664858bc9d3ded55502
Alexey Cherkasov <alcherkasov@gmail.com>
2021-09-03 09:46:37 +0500

    [step 2] Update automated help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,10 @@ import pckg from '../package.json';
 import genDiff from '../index.js';
 
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
   .version(pckg.version)
   .parse();
 
