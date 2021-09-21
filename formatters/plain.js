2f15d3a6caa8cdc752cd1a498eb8cb14779797f3
Alexey Cherkasov <alcherkasov@gmail.com>
2021-09-21 20:48:07 +0500

    [step 7] Add plain formatter

---
+++ formatters/plain.js
@@ -0,0 +1,61 @@
+import _ from 'lodash';
+
+const getOutputValue = (value) => {
+  if (typeof value === 'object' && value !== null) {
+    return '[complex value]';
+  }
+
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+
+  return value;
+};
+
+const plain = (ast) => {
+  const iter = (tree, path = []) => {
+    const keys = _.keys(tree);
+
+    const data = keys.reduce((acc, dataKey) => {
+      const node = tree[dataKey];
+
+      const {
+        state,
+        value,
+        oldValue,
+        children,
+      } = node;
+
+      const propertyPath = path.concat(dataKey);
+
+      if (children) {
+        return `${acc}${iter(children, propertyPath)}`;
+      }
+
+      const property = `${propertyPath.join('.')}`;
+
+      switch (state) {
+        case 'added': {
+          return `${acc}Property '${property}' was added with value: ${getOutputValue(value)}\n`;
+        }
+        case 'deleted': {
+          return `${acc}Property '${property}' was removed\n`;
+        }
+        case 'changed': {
+          return `${acc}Property '${property}' was updated. From ${getOutputValue(oldValue)} to ${getOutputValue(value)}\n`;
+        }
+        default: {
+          return acc;
+        }
+      }
+    }, '');
+
+    return data;
+  };
+
+  const result = iter(ast).trim();
+
+  return result;
+};
+
+export default plain;
