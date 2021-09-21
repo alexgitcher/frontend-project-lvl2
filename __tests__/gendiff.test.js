b540eadb48779174d9d395288e52b52692fbc4e0
Alexey Cherkasov <alcherkasov@gmail.com>
2021-09-21 08:57:32 +0500

    [step 7] Update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,45 +1,88 @@
 import { readFileSync } from 'fs';
-import { test, expect } from '@jest/globals';
+import { describe, test, expect } from '@jest/globals';
 
 import getFilePath from '../src/utils.js';
 
 import genDiff from '../src/gendiff.js';
 
-const resultFilepath = getFilePath('result.txt');
-const expected = readFileSync(resultFilepath, 'utf-8').trim();
+describe('check stylish diff', () => {
+  const resultFilepath = getFilePath('stylish-result.txt');
+  const expected = readFileSync(resultFilepath, 'utf-8').trim();
 
-test('check json diff', () => {
-  const filepath1 = getFilePath('file1.json');
-  const filepath2 = getFilePath('file2.json');
+  test('json diff', () => {
+    const filepath1 = getFilePath('file1.json');
+    const filepath2 = getFilePath('file2.json');
 
-  const actual = genDiff(filepath1, filepath2);
+    const actual = genDiff(filepath1, filepath2);
 
-  expect(actual).toEqual(expected);
+    expect(actual).toEqual(expected);
+  });
+
+  test('yaml diff', () => {
+    const filepath1 = getFilePath('file1.yaml');
+    const filepath2 = getFilePath('file2.yaml');
+
+    const actual = genDiff(filepath1, filepath2);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('yml diff', () => {
+    const filepath1 = getFilePath('file1.yml');
+    const filepath2 = getFilePath('file2.yml');
+
+    const actual = genDiff(filepath1, filepath2);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('json & yaml diff', () => {
+    const filepath1 = getFilePath('file1.json');
+    const filepath2 = getFilePath('file2.yaml');
+
+    const actual = genDiff(filepath1, filepath2);
+
+    expect(actual).toEqual(expected);
+  });
 });
 
-test('check yaml diff', () => {
-  const filepath1 = getFilePath('file1.yaml');
-  const filepath2 = getFilePath('file2.yaml');
+describe('check plain diff', () => {
+  const resultFilepath = getFilePath('plain-result.txt');
+  const expected = readFileSync(resultFilepath, 'utf-8').trim();
 
-  const actual = genDiff(filepath1, filepath2);
+  test('json diff', () => {
+    const filepath1 = getFilePath('file1.json');
+    const filepath2 = getFilePath('file2.json');
 
-  expect(actual).toEqual(expected);
-});
+    const actual = genDiff(filepath1, filepath2, 'plain');
 
-test('check yml diff', () => {
-  const filepath1 = getFilePath('file1.yml');
-  const filepath2 = getFilePath('file2.yml');
+    expect(actual).toEqual(expected);
+  });
 
-  const actual = genDiff(filepath1, filepath2);
+  test('yaml diff', () => {
+    const filepath1 = getFilePath('file1.yaml');
+    const filepath2 = getFilePath('file2.yaml');
 
-  expect(actual).toEqual(expected);
-});
+    const actual = genDiff(filepath1, filepath2, 'plain');
 
-test('check json & yaml diff', () => {
-  const filepath1 = getFilePath('file1.json');
-  const filepath2 = getFilePath('file2.yaml');
+    expect(actual).toEqual(expected);
+  });
 
-  const actual = genDiff(filepath1, filepath2);
+  test('yml diff', () => {
+    const filepath1 = getFilePath('file1.yml');
+    const filepath2 = getFilePath('file2.yml');
 
-  expect(actual).toEqual(expected);
+    const actual = genDiff(filepath1, filepath2, 'plain');
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('json & yaml diff', () => {
+    const filepath1 = getFilePath('file1.json');
+    const filepath2 = getFilePath('file2.yaml');
+
+    const actual = genDiff(filepath1, filepath2, 'plain');
+
+    expect(actual).toEqual(expected);
+  });
 });
