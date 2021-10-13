0ad673b52d786d4544f3eee021be1d3da5e2f3dd
Alexey Cherkasov <alcherkasov@gmail.com>
2021-10-13 12:16:36 +0500

    Build AST refactoring

--- src/build-ast.js
+++ src/build-ast.js
@@ -1,74 +1,53 @@
 import _ from 'lodash';
 
+const getKeyState = (obj1, obj2, key) => {
+  const val1 = obj1[key];
+  const val2 = obj2[key];
+
+  switch (true) {
+    case _.isObject(val1) && _.isObject(val2):
+      return 'unchanged';
+    case _.has(obj1, key) && _.has(obj2, key) && val1 !== val2:
+      return 'changed';
+    case !val1:
+      return 'added';
+    case !val2:
+      return 'deleted';
+    default:
+      return 'unchanged';
+  }
+};
+
+const buildNode = ({ acc, props }) => ({ ...acc, [props.key]: { ...props } });
+
 const buildAst = (obj1, obj2, depth = 1) => {
   const uniqKeys = _.union(_.keys(obj1), _.keys(obj2));
   const sortedKeys = _.sortBy(uniqKeys);
 
-  const ast = sortedKeys.reduce((acc, key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
+  return sortedKeys.reduce((acc, key) => {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+    const state = getKeyState(obj1, obj2, key);
+    const baseProps = { key, state, depth };
 
-    if (_.isObject(value1) && _.isObject(value2)) {
-      return {
-        ...acc,
-        [key]: {
-          key,
-          state: 'unchanged',
-          depth,
-          children: buildAst(value1, value2, depth + 1),
-        },
-      };
+    switch (state) {
+      case 'changed':
+        return buildNode({ acc, props: { ...baseProps, value: val2, oldValue: val1 } });
+      case 'added':
+        return buildNode({ acc, props: { ...baseProps, value: val2 } });
+      case 'deleted':
+        return buildNode({ acc, props: { ...baseProps, value: val1 } });
+      default: {
+        return buildNode({
+          acc,
+          props: {
+            ...baseProps,
+            ...(_.isObject(val1) ? { children: buildAst(val1, val2, depth + 1) } : { value: val1 }),
+          },
+        });
+      }
     }
-
-    if ((_.has(obj1, key) && _.has(obj2, key)) && value1 !== value2) {
-      return {
-        ...acc,
-        [key]: {
-          key,
-          value: value2,
-          oldValue: value1,
-          depth,
-          state: 'changed',
-        },
-      };
-    }
-
-    if (value1 === undefined) {
-      return {
-        ...acc,
-        [key]: {
-          key,
-          value: value2,
-          depth,
-          state: 'added',
-        },
-      };
-    }
-
-    if (value2 === undefined) {
-      return {
-        ...acc,
-        [key]: {
-          key,
-          value: value1,
-          depth,
-          state: 'deleted',
-        },
-      };
-    }
-
-    return {
-      ...acc,
-      [key]: {
-        key,
-        value: value1,
-        depth,
-        state: 'unchanged',
-      },
-    };
   }, {});
-
-  return ast;
 };
 
 export default buildAst;
